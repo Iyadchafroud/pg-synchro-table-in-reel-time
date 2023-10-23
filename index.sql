706d7ff3a3d8a3f9d2d9844d8ac8502c0b82db36
mohamed iyed chafroud <60520088+Iyadchafroud@users.noreply.github.com>
2023-10-23 15:03:00 +0100

    feat: add pg_stat_subscription

--- index.sql
+++ index.sql
@@ -6,12 +6,14 @@ CREATE SUBSCRIPTION sub_name CONNECTION 'dbname=postgres host=172.17.0.2 user=po
 
 -- now when i modify  or insert line , it make the synch in reel time  in the specific table
 
-select * from pg_stat_replication_slots
-
+--The pg_stat_subscription  view will contain one row per logical subscription
+select * from pg_stat_subscription
+  
 --The pg_stat_replication_slots view will contain one row per logical replication slot, showing statistics about its usage.
-
-select * from pg_stat_wal_receiver 
+select * from pg_stat_replication_slots
+  
 -- The pg_stat_wal_receiver view will contain only one row, showing statistics about the WAL receiver from that receiver's connected server.
+select * from pg_stat_wal_receiver 
 
 
 
